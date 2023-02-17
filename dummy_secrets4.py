1a05453cb52dd0a094bf65b1e6e54b7b28513527
Muhammad Shahid <61520009+ZayanShahid@users.noreply.github.com>
2023-02-17 11:30:13 +0500

    Add files via upload

---
+++ dummy_secrets4.py
@@ -0,0 +1,60 @@
+API keys:
+
+Google Maps API key: AIzaSyA-xxxxxxx...xxxxxxxQ
+Stripe API key: sk_test_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+Twilio API key: ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+Database credentials:
+
+MySQL database credentials:
+Hostname: mysql.example.com
+Database name: my_database
+Username: my_username
+Password: my_password
+PostgreSQL database credentials:
+Hostname: postgresql.example.com
+Database name: my_database
+Username: my_username
+Password: my_password
+Encryption keys:
+
+AES encryption key: tLpAStxmpJjKtvxyGmcX9tF4dKjMpStF
+RSA encryption key: -----BEGIN RSA PRIVATE KEY----- MIIEowIBAAKCAQEA2e4Z3K0bivLeYih0w1N+xzKXd5IJRi0R0/5L9b5FhEo... -----END RSA PRIVATE KEY-----
+Passwords:
+
+User password: my_password
+Admin password: my_admin_password
+Tokens:
+
+JWT token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...
+OAuth2 access token: ya29.a0AfH6SMAkUJGF...
+Other secrets:
+
+SSH private key: '-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9u-----END OPENSSH PRIVATE KEY-----'
+
+
+
+API keys:
+
+GOOGLE_API_KEY: "AIzaSyB0K0vLk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+TWILIO_API_KEY: "ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+SLACK_API_KEY: "xoxb-xxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxx"
+Access tokens:
+
+GITHUB_ACCESS_TOKEN: "ghp_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+DROPBOX_ACCESS_TOKEN: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+FACEBOOK_ACCESS_TOKEN: "EAAGxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+Usernames:
+
+GITHUB_USERNAME: "john-doe"
+TWITTER_USERNAME: "johndoe123"
+INSTAGRAM_USERNAME: "johndoe_official"
+Passwords:
+
+EMAIL_PASSWORD: "myEmailPassword1234"
+DATABASE_PASSWORD: "myDatabasePassword5678"
+WIFI_PASSWORD: "myWiFiPassword91011"
+Other secrets:
+
+SECRET_KEY: "mySecretKey1234567890"
+PRIVATE_KEY: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC6Fkwq3T\nwxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n4d3Jc8x+2kGgRZeR23O30zSb8FpgrGzvV7OZhTVKM5uV7Wk9+XXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\nXXXXXXXXXXXXXX\n-----END PRIVATE KEY-----"
---
+++ dummy_secrets5.py
@@ -0,0 +1,11 @@
+Social Security Number: 123-45-6789
+Credit Card Number: 4111 1111 1111 1111
+Bank Account Number: 1234567890
+Passport Number: ABC123456
+Driver's License Number: 1234567ABCD
+Email Address: john.doe@example.com
+Phone Number: +1 (555) 555-5555
+IP Address: 192.168.0.1
+URL: https://example.com
+API Key: abcdef1234567890
+Access Token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
