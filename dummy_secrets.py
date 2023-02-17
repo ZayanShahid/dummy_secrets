f04e8d4393f5b7a44a1447a404b0e6f73b73e66d
Muhammad Shahid <61520009+ZayanShahid@users.noreply.github.com>
2023-02-17 11:24:55 +0500

    dummy secrets uploaded

---
+++ dummy_secrets.py
@@ -0,0 +1,47 @@
+Adafruit IO Adafruit IO Key 1234567890123456789012345678901234567890
+Adobe Adobe Device Token abcdefghijklmnopqrstuvwxyz0123456789
+Adobe Adobe Service Token abcdefghijklmnopqrstuvwxyz0123456789
+Adobe Adobe Short-Lived Access Token abcdefghijklmnopqrstuvwxyz0123456789
+Adobe Adobe JSON Web Token abcdefghijklmnopqrstuvwxyz0123456789
+Alibaba Cloud Alibaba Cloud Access Key ID and Access Key Secret pair accesskeyid123:accesskeysecret456
+Amazon Web Services (AWS) Amazon AWS Access Key ID and Secret Access Key pair accesskeyid123:accesskeysecret456
+Atlassian Atlassian API Token abcdefghijklmnopqrstuvwxyz0123456789
+Atlassian Atlassian JSON Web Token abcdefghijklmnopqrstuvwxyz0123456789
+Azure Azure Active Directory Application Secret abcdefghijklmnopqrstuvwxyz0123456789
+Azure Azure Batch Key Identifiable abcdefghijklmnopqrstuvwxyz0123456789
+Azure Azure CosmosDB Key Identifiable abcdefghijklmnopqrstuvwxyz0123456789
+Azure Azure DevOps Personal Access Token abcdefghijklmnopqrstuvwxyz0123456789
+Azure Azure ML Studio (classic) Web Service Key abcdefghijklmnopqrstuvwxyz0123456789
+Azure Azure SAS Token abcdefghijklmnopqrstuvwxyz0123456789
+Azure Azure Search Admin Key abcdefghijklmnopqrstuvwxyz0123456789
+Azure Azure Search Query Key abcdefghijklmnopqrstuvwxyz0123456789
+Azure Azure Service Management Certificate abcdefghijklmnopqrstuvwxyz0123456789
+Azure Azure SQL Connection String abcdefghijklmnopqrstuvwxyz0123456789
+Azure Azure Storage Account Key abcdefghijklmnopqrstuvwxyz0123456789
+Checkout.com Checkout.com Production Secret Key sk_test_1234567890123456789012345678901234567890
+Checkout.com Checkout.com Test Secret Key pk_test_1234567890123456789012345678901234567890
+Chief Tools Chief Tools Token abcdefghijklmnopqrstuvwxyz0123456789
+Clojars Clojars Deploy Token abcdefghijklmnopqrstuvwxyz0123456789
+CloudBees CodeShip CloudBees CodeShip Credential abcdefghijklmnopqrstuvwxyz0123456789
+Contributed Systems Contributed Systems Credentials abcdefghijklmnopqrstuvwxyz0123456789
+Crates.io Crates.io API Token abcdefghijklmnopqrstuvwxyz0123456789
+Databricks Databricks Access Token abcdefghijklmnopqrstuvwxyz0123456789
+Datadog Datadog API Key abcdefghijklmnopqrstuvwxyz0123456789
+DevCycle DevCycle Client API Key abcdefghijklmnopqrstuvwxyz0123456789
+DevCycle DevCycle Server API Key abcdefghijklmnopqrstuvwxyz0123456789
+DigitalOcean DigitalOcean Personal Access Token abcdefghijklmnopqrstuvwxyz0123456789
+DigitalOcean DigitalOcean OAuth Token abcdefghijklmnopqrstuvwxyz0123456789
+DigitalOcean DigitalOcean Refresh Token abcdefghijklmnopqrstuvwxyz0123456789
+DigitalOcean DigitalOcean System Token abcdefghijklmnopqrstuvwxyz0123456789
+Discord Discord Bot Token abcdefghijklmnopqrstuvwxyz0123456789
+Doppler Doppler Personal Token abcdefghijklmnopqrstuvwxyz0123456789
+Doppler Doppler Service Token abcdefghijklmnopqrstuvwxyz0123456789
+Doppler Doppler CLI Token abcdefghijklmnopqrstuvwxyz0123456789
+Doppler Doppler SCIM Token abcdefghijklmnopqrstuvwxyz0123456789
+Doppler Doppler Audit Token abcdefghijklmnopqrstuvwxyz0123456789
+Dropbox Dropbox Access Token abcdefghijklmnopqrstuvwxyz0123456789
+Dropbox Dropbox Short Lived Access Token abcdefghijklmnopqrstuvwxyz0123456789
+Dynatrace Dynatrace Access Token abcdefghijklmnopqrstuvwxyz0123456789
+Dynatrace Dynatrace Internal Token abcdefghijklmnopqrstuvwxyz0123456789
+Figma Figma Personal Access Token abcdefghijklmnopqrstuvwxyz0123456789
+Finicity Finicity App Key abcdefghijklmnopqrstuvwxyz0123456789
